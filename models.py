109bbf42440dabc9b41a4b069f29700984a3452d
sduplantis18 <sduplantis18@gmail.com>
2020-08-02 17:22:18 -0400

    added foreign keys to all models

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 import os
 from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 import json
 
 database_name = "suppliers"
-database_path = f'postgresql://sdupl:Baller24@localhost:5432/{database_name}'
+database_path = f'postgresql://sdupl:#####@localhost:5432/{database_name}'
 
 db = SQLAlchemy()
 
@@ -24,6 +25,8 @@ class Arena(db.Model):
     id = Column(Integer, primary_key = True)
     name = Column(String)
     address = Column(String)
+    restaurant = relationship('Restaurant', backref='arena', lazy=True)
+    runner = relationship('Runner', backref='runner', Lazy=True)
 
 '''
 Define Restaurant Class
@@ -33,6 +36,10 @@ class Restuarant(db.Model):
     id = Column(Integer, primary_key = True)
     name = Column(String)
     address = Column(String)
+    arena_id = Column(Integer, ForeignKey('Arena.id'), nullable = False)
+    menu = relationship('Menu', backref='menu', lazy = True)
+    customer = relationship('Customer', backref='customer', lazy = True)
+    order = relationship('Order', backref='order', lazy = True)
 
 '''
 Define menu class
@@ -40,6 +47,8 @@ Define menu class
 class Menu(db.Model):
     __tablename__ = 'menus'
     id = Column(Integer, primary_key = True)
+    restaurant_id = Column(Integer, ForeignKey('Restuarant.id') nullable = False)
+    menu_item = relationship('Menu_Item', backref='menu_item', lazy = True)
 
 '''
 Define menu item class
@@ -48,6 +57,7 @@ class Menu_Item(db.Model):
     __tablename__ = 'menu_items'
     id = Column(Integer, primary_key = True)
     name = Column(String)
+    menu_id = Column(Integer, ForeignKey('Menu.id') nullable = False)
 
 '''
 Define Customer class
@@ -57,6 +67,8 @@ class Customer(db.Model):
     id = Column(Integer, primary_key = True)
     name = Column(String)
     phone_number = Column(String(15))
+    restaurant_id = Column(Integer, ForeignKey('Restuarant.id') nullable = False)
+    order = relationship('Order', backref='order', lazy = True)
 
 '''
 Define runner class
@@ -64,6 +76,8 @@ Define runner class
 class Runner(db.Model):
     __tablename__ = 'runners'
     id = Column(Integer, primary_key = True)
+    arena_id = Column(Integer, ForeignKey('Arena.id'), nullable = False)
+    shipment = relationship('Shipment', backref='shipment', lazy = True)
 
 '''
 Define Order class
@@ -73,6 +87,9 @@ class Order(db.Model):
     id = Column(Integer, primary_key=True)
     order_date = Column(DateTime)
     order_type = Column(String)
+    customer_id = Column(Integer, ForeignKey('Customer.id') nullable = False)
+    restaurant_id = Column(Integer, ForiegnKey('Restaurant.id') nullable =False)
+    shipment = relationship('Shipment', backref='shipment', lazy = True)
 
 '''
 Define shipment class
@@ -81,6 +98,8 @@ class Shipment(db.Model):
     __tablename__ = 'shipments'
     id = Column(Integer, primary_key = True)
     shipment_date = Column(DateTime)
+    order_id = Column(Integer, ForeignKey('Order.id') nullable = False)
+    runner_id = Column(Integer, ForeignKey('Runner.id') nullable = False)
 
 
 
